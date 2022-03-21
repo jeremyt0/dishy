d72ed6ce7ef91d0f336d4705e7ccf8d8519e3edb
jeremyt0 <jez_is_the_best@hotmail.com>
2022-03-21 00:32:08 +0000

    Change default path

--- dish-segmentor/main.py
+++ dish-segmentor/main.py
@@ -3,6 +3,7 @@ from utilities.logg import LOGGER
 
 if __name__=="__main__":
     LOGGER.debug("Starting application...")
-    path = "../images"
+    # path = "../images"  # Mac
+    path = ""
     Main.main(path=path)
     LOGGER.success("End of program.")
