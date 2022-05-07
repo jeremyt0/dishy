e63f2540b5011c83e753a27f6e64013cc0c63c4f
jeremyt0 <jez_is_the_best@hotmail.com>
2022-05-07 13:44:52 +0100

    Try catch for main and pause after program exits

--- dish-segmentor/main.py
+++ dish-segmentor/main.py
@@ -1,9 +1,15 @@
 from dishy import Main
 from utilities.logg import LOGGER
 
+import os
+
 if __name__=="__main__":
-    LOGGER.debug("Starting application...")
-    # path = "../images"  # Mac
-    path = ""
-    Main.main(path=path)
-    LOGGER.success("End of program.")
+    try:
+        LOGGER.debug("Starting application...")
+        # path = "../images"  # Mac
+        path = ""
+        Main.main(path=path)
+        LOGGER.success("End of program.")
+    except Exception as e:
+        print(e)
+    os.system('pause')
